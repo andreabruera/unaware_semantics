68dbf6115362a84f3ee2abc0b09d63124700f9f3
Andrea <andrea.bruera@gmail.com>
2023-09-20 14:41:18 +0100

    new plots

--- 04_rsa_simple.py
+++ 04_rsa_simple.py
@@ -47,6 +47,7 @@ def compute_ranking(t):
     accuracies = list()
     for test_item, real_vec in t_data.items():
 
+        '''
         ### remove average activity
         avg_data = numpy.average([v for k, v in t_data.items() if k!=test_item], axis=0)
         current_data = {k : v-avg_data for k, v in t_data.items()}
@@ -56,7 +57,6 @@ def compute_ranking(t):
         ### leaves ERPs untouched
         pred = numpy.sum([t_data[w]*similarities[model][tuple(sorted([w, test_item]))] for w in t_data.keys() if w!=test_item], axis=0)
         scores = {w : scipy.stats.pearsonr(erp, pred)[0] for w, erp in t_data.items()}
-        '''
         ### sorting and looking at ranking
         sorted_w = [v[0] for v in sorted(scores.items(), key=lambda item : item[1], reverse=True)]
         rank = 1 - (sorted_w.index(test_item) / len(sorted_w))
@@ -266,12 +266,14 @@ for model in [
                                     eeg_f,
                                     verbose=False,
                                     preload=True)
+            '''
             s_data = raw_f.get_data(picks='eeg')
-            #s_data_unscaled = raw_f.get_data(picks='eeg')
+            '''
+            s_data_unscaled = raw_f.get_data(picks='eeg')
             ### Scaling 
-            #s_data = mne.decoding.Scaler(raw_f.info, \
-            #            scalings='mean'\
-            #            ).fit_transform(s_data_unscaled)
+            s_data = mne.decoding.Scaler(raw_f.info, \
+                        scalings='mean'\
+                        ).fit_transform(s_data_unscaled)
             xs = raw_f.times
             events = raw_f.events
             ### initializing ERPs
